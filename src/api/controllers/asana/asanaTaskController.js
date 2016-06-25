a8ee7aa6ce7896b13acdca9bfd81aa1b0cfa39ad
Craig-Qubit <craig@qubit.com>
2016-06-25 18:36:03 +0100

    More asanaTask data returned for list - limit to 10 for dev speed

--- src/api/controllers/asana/asanaTaskController.js
+++ src/api/controllers/asana/asanaTaskController.js
@@ -2,7 +2,7 @@
 * @Author: craigbojko
 * @Date:   2016-04-18T16:18:53+01:00
 * @Last modified by:   craigbojko
-* @Last modified time: 2016-04-24T16:09:24+01:00
+* @Last modified time: 2016-06-21T17:03:18+01:00
 */
 
 var Asana = require('asana')
@@ -23,15 +23,18 @@ function getAsanaTasksWithProjectId () {
   return new Promise(function (resolve, reject) {
     asana.tasks.findByProject('32972561372564', {
       workspace: '896401739841',
-      opt_fields: 'name,projects.id,projects.name'
+      opt_fields: 'id,name,current_status,projects.id,projects.name'
     }).then(function (collection) {
-      collection.fetch(1000).then(function (tasks) {
+      collection.fetch(10).then(function (tasks) { // TODO up to 1000
         var parsedTasks = []
         for (var i = 0; i < tasks.length; i++) {
           var task = tasks[i]
           var _task = {}
+          var url = 'https://app.asana.com/0/32972561372564/' + task.id
 
+          _task.id = task.id
           _task.name = task.name
+          _task.url = url
           if (task.projects && task.projects.length) {
             for (var j = 0; j < task.projects.length; j++) {
               var projectId = task.projects[j].id
